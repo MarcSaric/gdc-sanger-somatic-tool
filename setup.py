5000e51815bb357d57ebfc25f6602db3913429c6
Charles Czysz <charles.czysz@gmail.com>
2020-05-06 16:03:03 -0500

    Extended to write requirements

--- setup.py
+++ setup.py
@@ -1,4 +1,39 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, Command
+import os.path
+
+INSTALL_REQUIRES = [
+    'pysam',
+]
+
+DEV_REQUIRES = [
+    'pytest',
+    'pytest-cov',
+    'flake8',
+    'mock',
+]
+
+
+class Requirements(Command):
+    descripton = 'foobar'
+    user_options = [
+        ('dev', None, 'Bundles all requirements'),
+    ]
+
+    def initialize_options(self):
+        self.dev = False
+
+    def finalize_options(self):
+        self.dev = True
+
+    def run(self):
+        if self.dev:
+            path = os.path.join('.', 'requirements.in')
+            self.write_requirements(path, INSTALL_REQUIRES + DEV_REQUIRES)
+        return
+
+    def write_requirements(self, path, reqs):
+        with open(path, 'w') as fh:
+            fh.write('\n'.join(reqs)+'\n')
 
 setup(
     name = "gdc-sanger-tools",
@@ -9,8 +44,13 @@ setup(
     license = "Apache 2.0",
     packages = find_packages(),
     python_requires='>=3.5',
+    install_requires=INSTALL_REQUIRES,
+    tests_require=DEV_REQUIRES,
     entry_points= {
         'console_scripts':
         ['gdc-sanger-tools=gdc_sanger_tools.__main__:main']
+    },
+    cmdclass={
+        'capture_requirements': Requirements,
     },
 )
