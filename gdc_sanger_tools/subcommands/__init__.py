7e9f54dc5b9a0fe3f1f988bfeb5b26414d529f77
Charles Czysz <charles.czysz@gmail.com>
2020-05-07 14:18:55 -0500

    Added command finder

--- gdc_sanger_tools/subcommands/__init__.py
+++ gdc_sanger_tools/subcommands/__init__.py
@@ -1,4 +1,19 @@
-from __future__ import absolute_import
+import importlib
+import os
+from types import SimpleNamespace
 
-from .base import Subcommand
-from .check_bam_header import CheckBamHeader
+import stringcase as sc
+
+
+def find_commands():
+    _, _, files = list(os.walk(os.path.dirname(__file__)))[0]
+    commands = {}
+    for file in files:
+        if file in ['base.py', '__init__.py']:
+            continue
+        if not file.endswith('.py'):
+            continue
+        command, _ = os.path.splitext(file)
+        mod = importlib.import_module('.' + command, package=__name__)
+        commands[command] = getattr(mod, sc.pascalcase(command))
+    return commands
