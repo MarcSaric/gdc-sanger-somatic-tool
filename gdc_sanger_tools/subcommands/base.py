ee34403f20d0693035af4c95066a776d4e502c1d
Charles Czysz <charles.czysz@gmail.com>
2020-05-07 12:08:34 -0500

    Improve base class classmethods

--- gdc_sanger_tools/subcommands/base.py
+++ gdc_sanger_tools/subcommands/base.py
@@ -5,11 +5,13 @@ from abc import ABCMeta, abstractmethod
 
 from gdc_sanger_tools.utils import ArgParserT, NamespaceT
 
+
 class Subcommand(metaclass=ABCMeta):
     @classmethod
     @abstractmethod
     def __add_arguments__(cls, parser: ArgParserT):
         """Add the arguments to the parser"""
+        raise NotImplementedError
 
     @classmethod
     @abstractmethod
@@ -18,6 +20,7 @@ class Subcommand(metaclass=ABCMeta):
         The default function when the subcommand is selected.  Returns None if
         executed successfully.
         """
+        raise NotImplementedError
 
     @classmethod
     def __get_description__(cls):
@@ -37,8 +40,8 @@ class Subcommand(metaclass=ABCMeta):
     def add(cls, subparsers: ArgParserT) -> ArgParserT:
         """Adds the given subcommand to the subparsers."""
         subparser = subparsers.add_parser(
-            name=cls.__tool_name__(),
-            description=cls.__get_description__())
+            name=cls.__tool_name__(), description=cls.__get_description__()
+        )
 
         cls.__add_arguments__(subparser)
         subparser.set_defaults(func=cls.main)
