296b82bcf6beacb1292f5ffbb49fc158b9fc048e
Charles Czysz <charles.czysz@gmail.com>
2020-05-07 14:24:42 -0500

    Use new command finder

--- gdc_sanger_tools/__main__.py
+++ gdc_sanger_tools/__main__.py
@@ -4,16 +4,25 @@ Main entrypoint for all gdc-sanger-tools.
 import argparse
 import datetime
 import sys
-
-from signal import signal, SIGPIPE, SIG_DFL
+from signal import SIG_DFL, SIGPIPE, signal
 
 from gdc_sanger_tools.logger import Logger
-from gdc_sanger_tools.subcommands import CheckBamHeader 
-
+from gdc_sanger_tools.subcommands import find_commands
 
 signal(SIGPIPE, SIG_DFL)
 
-def main(args=None, extra_subparser=None):
+
+def setup_parser(subcommands):
+    parser = argparse.ArgumentParser("GDC Sanger Tools")
+    subparser = parser.add_subparsers(dest="subcommand")
+
+    for command in subcommands:
+        command.setup_parser(subparser)
+
+    return parser
+
+
+def main(argv=None, extra_subparser=None):
     """
     The main method for gdc-sanger-tools. 
     """
@@ -27,18 +36,19 @@ def main(args=None, extra_subparser=None):
     subparsers = p.add_subparsers(dest="subcommand")
     subparsers.required = True
 
-    CheckBamHeader.add(subparsers=subparsers)
+    found_subcommands = find_commands()
+    subcommands = list(found_subcommands.values())
+    parser = setup_parser(subcommands)
 
-    if extra_subparser:
-        extra_subparser.add(subparsers=subparsers)
-
-    options = p.parse_args(args)
+    args = parser.parse_args(argv)
 
     # Run
-    options.func(options)
+    # options.func(options)
 
+    logger.info(args)
     # Finish
     logger.info("Finished!")
+
 
 if __name__ == '__main__':
     main()
